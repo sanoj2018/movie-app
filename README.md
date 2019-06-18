704bd50b80ccf4d80f37ecb22152e7365d49296f
Sanoj Kumar <sanoj.Kumar@nagra.com>
2019-06-18 22:25:53 +0530

    adding basic app skeleton

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a movie-app SPA which display movie with related movie 
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+
+const Index = () => {
+    return (
+        <div>
+            <h1>This is index page</h1>
+        </div>
+    )
+}
+
+
+export default Index;
